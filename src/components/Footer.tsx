16396197da5645bfde3ab66584c790cbbeb0d5e2
sanjith <sanjith@tutamail.com>
2026-05-11 17:25:14 +0530

    feat(ui): Add GitHub icon to footer
    Introduce react-icons dependency and import SiGithub in Footer
    Update package.json and package-lock.json and adjust footer markup
    and classes for proper icon alignment and styling

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,28 +1,31 @@
-// src/components/Footer.tsx
+import { SiGithub } from "react-icons/si";
 
 export default function Footer() {
   return (
     <footer className="mt-16 py-4 bg-[#1d1d1d] text-sm text-center text-[#cecece] border-t border-[#202020] w-full">
-      <p>
-        Made with 💔 by{" "}
-        <a
-          href="https://github.com/s4nj1th/"
-          className="text-[#dedede] hover:underline hover:text-white"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Sanjith
-        </a>{" "}
-        ·{" "}
+      <p className="flex gap-2.5 justify-center">
+        <span>
+          Made with 💔 by{" "}
+          <a
+            href="https://github.com/s4nj1th/"
+            className="text-[#ededed] hover:underline hover:text-white"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Sanjith
+          </a>
+        </span>
+        ·
         <a
           href="https://github.com/s4nj1th/accurate-clock"
-          className="text-[#dedede] hover:underline hover:text-white"
+          className="text-[#ededed] hover:underline hover:text-white gap-1 flex items-center"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Source
-        </a>{" "}
-        · MIT License
+          <SiGithub />
+          <span>Source</span>
+        </a>
+        ·<span>MIT License</span>
       </p>
     </footer>
   );
